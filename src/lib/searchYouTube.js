5fc2d25887c30e352e00dfb1b42e069eea28f3aa
khengschew <khengc@gmail.com>
2019-01-17 17:53:58 -0800

    Implement get request to pull YouTube videos by search terms

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,23 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      'maxResults': options.max,
+      'part': 'snippet, id',
+      'q': options.query,
+      'type': 'video',
+      'key': options.key,
+      'videoEmbeddable': 'true'
+    },
+    contentType: 'application/json',
+    success: (data) => (
+      callback(data.items)
+    ),
+    error: function(error) {
+      console.error('chatterbox: Failed to fetch messages', error);
+    }
+  });
 };
 
 export default searchYouTube;
